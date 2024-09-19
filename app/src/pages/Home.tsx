79cadb9cc2d08135a6242241611b213833840209
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-19 11:26:26 +0200

    added navbar refactor part 2

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,16 +1,6 @@
-//react
 import React from "react";
-
-//mui
 import Box from "@mui/material/Box";
-import { AppBar, Divider, Toolbar } from "@mui/material";
-
-//components
-import { Logo } from "../components/navbarFields/Logo";
-import { SearchField } from "../components/navbarFields/SearchField";
-import { Category } from "../components/navbarFields/Category";
-import { User } from "../components/navbarFields/User";
-import { ThemeSwitch } from "../components/navbarFields/ThemeSwitch";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
@@ -22,28 +12,7 @@ export const Home = () => {
           bgcolor: "background.default",
         }}
       >
-        {/* Box with nav elements */}
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <Logo />
-              <SearchField />
-              <Category />
-              <Divider
-                orientation="vertical"
-                flexItem
-                sx={{ marginRight: "10px", marginLeft: "10px" }}
-              />
-              <User />
-              <Divider
-                orientation="vertical"
-                flexItem
-                sx={{ marginRight: "15px" }}
-              />
-              <ThemeSwitch />
-            </Toolbar>
-          </AppBar>
-        </Box>
+        <Navbar />
       </Box>
     </>
   );
