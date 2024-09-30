28d5f5e8db4825c1c5269cc8670732c3f7b832fa
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-30 18:34:10 +0200

    added delivery method logic

--- app/src/components/PaymentMethod.tsx
+++ app/src/components/PaymentMethod.tsx
@@ -5,25 +5,46 @@ import CreditCardIcon from "@mui/icons-material/CreditCard";
 // import { type } from '../redux/store';
 import { useFormContext } from "react-hook-form";
 import { Address } from "../pages/Finalization";
+import { useState } from "react";
 
 export const PaymentMethod = () => {
+  const [addClicked, setAddClicked] = useState(false);
+  const [cardFields, setCardFields] = useState(false);
+
   const {
     register,
     formState: { errors },
+    setValue,
   } = useFormContext<Address>();
-  //   const [paymentMethod, setPaymentMethod] = useState<string>("");
 
-  //   const handlePayOnDelivery = (event: React.MouseEvent<HTMLButtonElement>) => {
-  //     const value = event.currentTarget.value;
-  //     setPaymentMethod(value);
-  //   };
+  //set pay on delivery method
+  const handlePayOnDelivery = () => {
+    setValue("payOnDelivery", true);
+    setValue("creditCard.CVV", null);
+    setValue("creditCard.date", null);
+    setValue("creditCard.number", null);
+    setCardFields(false);
+    setAddClicked(false);
+  };
+
+  //show card fields
+  const handleShowCardField = () => {
+    setValue("payOnDelivery", false);
+    setCardFields(true);
+    setAddClicked(false);
+  };
+
+  //set card
+  const handleAddCardButton = () => {
+    setCardFields(false);
+    setAddClicked(true);
+  };
 
   return (
     <>
       <Paper
         sx={{
           margin: "14px 7px 0 0",
-          //   height: "100vh",
         }}
       >
         <Typography variant="h5" sx={{ marginLeft: "15px", padding: "5px" }}>
@@ -33,7 +54,6 @@ export const PaymentMethod = () => {
       <Paper
         sx={{
           margin: "14px 7px 0 0",
-          //   height: "100vh",
         }}
       >
         <Box
@@ -45,8 +65,8 @@ export const PaymentMethod = () => {
             variant="outlined"
             value="payOnDelivery"
             startIcon={<AttachMoneyIcon />}
-            sx={{ width: "200px", marginRight: "10px" }}
-            // onClick={handlePayOnDelivery}
+            sx={{ width: "250px", padding: "15px", marginRight: "10px" }}
+            onClick={handlePayOnDelivery}
           >
             pay on delivery
           </Button>
@@ -54,72 +74,97 @@ export const PaymentMethod = () => {
           <Button
             variant="outlined"
             value="paymentCard"
-            sx={{ width: "200px" }}
+            sx={{ width: "250px", padding: "15px" }}
             startIcon={<CreditCardIcon />}
-            // onClick={handlePayOnDelivery}
+            onClick={handleShowCardField}
           >
             payment card
           </Button>
-          {/* CREDIT CARD NUMBER */}
-          <Box sx={{}}>
-            <Typography variant="h6" sx={{ marginTop: "30px" }}>
-              Enter your Card:
-            </Typography>
-            <TextField
-              sx={{ width: "400px" }}
-              type="number"
-              label="Credit card number"
-              variant="outlined"
-              margin="dense"
-              {...register("creditCard.number", {
-                minLength: {
-                  value: 14,
-                  message: "Credit card number required 14 characters",
-                },
-                required: "Credit card number is required",
-              })}
-              error={!!errors.creditCard?.number}
-              helperText={errors.creditCard?.number?.message?.toString()}
-            />
-            <Box>
-              <TextField
-                sx={{ marginRight: "10px" }}
-                label="Valid Thru"
-                variant="outlined"
-                margin="dense"
-                {...register("creditCard.date", {
-                  pattern: {
-                    value: /(0[1-9]|1[0-2])\/[0-9]{2}/,
-                    message: "Invalid date. Use MM/YY format.",
-                  },
-                  required: "Valid Thru is required",
-                })}
-                error={!!errors.creditCard?.date}
-                helperText={errors.creditCard?.date?.message?.toString()}
-              />
-              <TextField
-                type="password"
-                label="CVV"
-                variant="outlined"
-                margin="dense"
-                {...register("creditCard.CVV", {
-                  maxLength: {
-                    value: 3,
-                    message: "CVV must have 3 characters",
-                  },
-                  required: "CVV is required",
-                })}
-                error={!!errors.creditCard?.CVV}
-                helperText={errors.creditCard?.CVV?.message?.toString()}
-              />
-            </Box>
-            <Button
-              variant="contained"
-              sx={{ marginTop: "20px", width: "250px" }}
+          {addClicked ? (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}
             >
-              Add Card
-            </Button>
-          </Box>
+              <Typography variant="h6" sx={{ margin: "30px 0 15px 0" }}>
+                Card Added !
+              </Typography>
+              <Button
+                sx={{ margin: "15px 0 0 10px" }}
+                onClick={handleShowCardField}
+              >
+                Edit card
+              </Button>
+            </Box>
+          ) : null}
+
+          {/* CREDIT CARD NUMBER */}
+          {cardFields ? (
+            <Box sx={{}}>
+              <Typography variant="h6" sx={{ margin: "30px 0 15px 0" }}>
+                Enter your Card:
+              </Typography>
+
+              {/* CARD FIELDS */}
+              <TextField
+                sx={{ width: "400px" }}
+                type="number"
+                label="Credit card number"
+                variant="outlined"
+                margin="dense"
+                {...register("creditCard.number", {
+                  minLength: {
+                    value: 14,
+                    message: "Credit card number required 14 characters",
+                  },
+                  required: "Credit card number is required",
+                })}
+                error={!!errors.creditCard?.number}
+                helperText={errors.creditCard?.number?.message?.toString()}
+              />
+              <Box>
+                <TextField
+                  sx={{ marginRight: "10px" }}
+                  label="Valid Thru"
+                  variant="outlined"
+                  margin="dense"
+                  {...register("creditCard.date", {
+                    pattern: {
+                      value: /(0[1-9]|1[0-2])\/[0-9]{2}/,
+                      message: "Invalid date. Use MM/YY format.",
+                    },
+                    required: "Valid Thru is required",
+                  })}
+                  error={!!errors.creditCard?.date}
+                  helperText={errors.creditCard?.date?.message?.toString()}
+                />
+                <TextField
+                  type="password"
+                  label="CVV"
+                  variant="outlined"
+                  margin="dense"
+                  {...register("creditCard.CVV", {
+                    maxLength: {
+                      value: 3,
+                      message: "CVV must have 3 characters",
+                    },
+                    required: "CVV is required",
+                  })}
+                  error={!!errors.creditCard?.CVV}
+                  helperText={errors.creditCard?.CVV?.message?.toString()}
+                />
+              </Box>
+              <Button
+                variant="contained"
+                sx={{ marginTop: "20px", width: "250px" }}
+                onClick={handleAddCardButton}
+              >
+                Add Card
+              </Button>
+            </Box>
+          ) : null}
         </Box>
       </Paper>
     </>
