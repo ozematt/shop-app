3412f033514e5686ae056aae098c721a1193d8f0
Mateusz Obirek <neutralnie7@gmail.com>
2024-10-08 12:17:35 +0200

    added tests

---
+++ app/src/test/components/OrdersEmpty.spec.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+
+import { expect } from "vitest";
+import { MemoryRouter } from "react-router-dom";
+import { OrdersEmpty } from "../../components/ordersHistoryPage/OrdersEmpty";
+
+describe("<OrdersEmpty />", () => {
+  test("displaying a empty orders history message", () => {
+    render(
+      <MemoryRouter>
+        <OrdersEmpty />
+      </MemoryRouter>
+    );
+    const text = screen.getByText("You don't have any previous orders...");
+    expect(text).toBeInTheDocument();
+  });
+});
