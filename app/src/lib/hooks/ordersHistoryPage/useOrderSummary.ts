b2e755a2ca9e597ceeda95ddf805d1091a6ff4d1
Mateusz Obirek <neutralnie7@gmail.com>
2024-10-14 16:44:19 +0200

    added custom hook

--- app/src/lib/hooks/ordersHistoryPage/useOrderSummary.ts
+++ app/src/lib/hooks/ordersHistoryPage/useOrderSummary.ts
@@ -1,1 +1,8 @@
 import { useMediaQuery } from "@mui/material";
+
+export const useOrderSummary = () => {
+  //
+  ////DATA
+  const isSmallScreen = useMediaQuery("(max-width:790px)");
+  return { isSmallScreen };
+};
