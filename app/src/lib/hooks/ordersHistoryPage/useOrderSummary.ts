3e322aaef3a2face315646e497fbd507493ab04f
Mateusz Obirek <neutralnie7@gmail.com>
2024-10-18 18:12:26 +0200

    deleted custom hook

--- app/src/lib/hooks/ordersHistoryPage/useOrderSummary.ts
+++
@@ -1,8 +0,0 @@
-import { useMediaQuery } from "@mui/material";
-
-export const useOrderSummary = () => {
-  //
-  ////DATA
-  const isSmallScreen = useMediaQuery("(max-width:790px)");
-  return { isSmallScreen };
-};
