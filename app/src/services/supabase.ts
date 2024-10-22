04eebf1e83fe5c554993673edb54cb8f178a66e9
Mateusz Obirek <neutralnie7@gmail.com>
2024-10-22 08:31:34 +0200

    added types in supabase

--- app/src/services/supabase.ts
+++ app/src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_KEY as string;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
