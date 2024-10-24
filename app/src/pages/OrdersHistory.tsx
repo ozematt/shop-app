ea9e4af76ed83b5415a6f6587f97a637fc81cba9
Mateusz Obirek <neutralnie7@gmail.com>
2024-10-24 18:39:35 +0200

    added component

--- app/src/pages/OrdersHistory.tsx
+++ app/src/pages/OrdersHistory.tsx
@@ -1,2 +1,34 @@
+import { Container, CssBaseline, Paper, Typography } from "@mui/material";
+import { OrderItem } from "../components/ordersHistoryPage/OrderItem";
+import { OrdersEmpty } from "../components/ordersHistoryPage/OrdersEmpty";
+import { useOrdersHistory } from "../lib/hooks/pages/useOrdersHistory";
 
+export const OrdersHistory = () => {
+  //
+  ////DATA
+  const { orders } = useOrdersHistory();
+
+  ////UI
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Paper
+          sx={{
+            marginTop: "90px",
+          }}
+        >
+          <Typography variant="h5" sx={{ padding: "20px" }}>
+            Yours orders history:
+          </Typography>
+        </Paper>
+
+        {orders.length > 0 ? (
+          orders.map((order) => <OrderItem key={order.id} order={order} />)
+        ) : (
+          <OrdersEmpty />
+        )}
+      </Container>
+    </>
+  );
 };
