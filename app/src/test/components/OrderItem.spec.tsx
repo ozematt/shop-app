044cde07d557423aa22ce6b0fafeb356ee9e0a30
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-05 17:38:14 +0100

    edited import

--- app/src/test/components/OrderItem.spec.tsx
+++ app/src/test/components/OrderItem.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { expect } from "vitest";
 import { OrderItem } from "../../components/ordersHistoryPage/OrderItem";
-import { Orders } from "../../lib/types/ordersTypes";
+import { type Orders } from "../../lib/types";
 //
 ////TESTS
 describe("<OrderItem />", () => {
