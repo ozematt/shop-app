7d4ebafd60fe5e5d4f7e550a80e862ac3dcc9dee
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-05 17:08:22 +0100

    added types

--- app/src/lib/types.ts
+++ app/src/lib/types.ts
@@ -1,3 +1,32 @@
+type OrdersAddress = {
+  name: string;
+  surname: string;
+  street: string;
+  houseNumber: number | null;
+  apartmentNumber: number | null;
+  zipCode: string | null;
+  city: string;
+  payOnDelivery: boolean | null;
+  paymentCard: boolean | null;
+};
+
+export type OrdersItem = {
+  id: number | null;
+  title: string;
+  image: string;
+  price: number;
+  pieces: number;
+};
+
+export type Orders = {
+  id: string | null;
+  date: string | null;
+  totalPrice: number | null;
+  quantity: number | null;
+  address: OrdersAddress;
+  items: OrdersItem[];
+};
+
 type Rating = {
   rate: number;
   count: number;
