bc4a8e6e952fbc9075860ec276aa554a285063c1
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-05 17:09:58 +0100

    added types

--- app/src/lib/types.ts
+++ app/src/lib/types.ts
@@ -1,3 +1,28 @@
+export type Address = {
+  name: string;
+  surname: string;
+  email: string;
+  phone: number | null;
+  street: string;
+  houseNumber: number | null;
+  apartmentNumber: number | null;
+  zipCode: string | null;
+  city: string;
+  payOnDelivery: boolean | null;
+  paymentCard: boolean | null;
+  cardNumber: number | null;
+  cardDate: string | null;
+  cardCVV: string | null;
+};
+
+export type CartProduct = {
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+  pieces: number;
+};
+
 type OrdersAddress = {
   name: string;
   surname: string;
